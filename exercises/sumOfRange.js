fe28fbf8efc553c1d2823c06a8572b1fb6d5de70
charlieperson <charlie.person@hyperisland.se>
2016-06-20 13:57:13 -0500

    Adds interval functionality to range function

--- exercises/sumOfRange.js
+++ exercises/sumOfRange.js
@@ -1,7 +1,21 @@
-function range(start, end){
+function range(start, end, interval = 1){
   var solution = [];
-  for(i=start; i<end+1; i++){
-    solution.push(i);
+  if (start < end) {
+    for(i=start; i<end+1; i += interval){
+      solution.push(i);
+    }
+  } else {
+    for(i=start; i>end-1; i += interval){
+      solution.push(i);
+    }
   }
   return solution;
+}
+
+function sum(array){
+  var sum = 0;
+  for(i=0; i<array.length; i++) {
+    sum += array[i];
+  }
+  return sum;
 }
