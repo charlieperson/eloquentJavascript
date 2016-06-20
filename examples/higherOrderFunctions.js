307fe0cf09a17ee24585a09a173e5c27e25eaa1e
charlieperson <charlie.person@hyperisland.se>
2016-06-20 18:36:26 -0500

    Adds noisy function to higherOrderFunction examples file

--- examples/higherOrderFunctions.js
+++ examples/higherOrderFunctions.js
@@ -1,3 +1,12 @@
 function greaterThan(n) {
   return function(m) { return m > n; };
 }
+
+function noisy(f) {
+  return function(arg) {
+    console.log("calling with", arg);
+    var val = f(arg);
+    console.log('called with', arg, '- got', val);
+    return val;
+  };
+}
