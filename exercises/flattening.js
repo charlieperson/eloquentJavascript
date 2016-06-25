2989002d4996aeabb49b05830e428321ba459897
charlieperson <charlie.person@hyperisland.se>
2016-06-25 15:55:40 -0500

    Adds flatten exercise from chapter 5- completed

--- exercises/flattening.js
+++ exercises/flattening.js
@@ -1,8 +1,7 @@
 var arrays = [[1, 2, 3], [4, 5], [6]];
 
-function flatten(array) {
-  array.reduce(function(a, b){
-    a = a.concat(b);
-  });
-  return a;
+function flatten(arrays) {
+  console.log(arrays.reduce(function(a, b){
+    return a.concat(b);
+  }));
 }
